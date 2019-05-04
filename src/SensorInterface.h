8bb93ee9d564f7cbad626f7f3ff9bc5557486471
Reid Miller <sweetreid@gmail.com>
2019-05-04 00:08:52 -0400

    Abstract interface into class.

---
+++ src/SensorInterface.h
@@ -0,0 +1,35 @@
+/*
+  Buttoneer <SensorInterfaces.h>
+
+  Copyright (c) 2019, Reid Miller
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef SensorInterfaces_h
+#define SensorInterfaces_h
+
+#include "Arduino.h"
+
+class SensorInterface{};
+class EncoderInterface:public SensorInterface{
+  public:
+    int pinL;
+    int pinR;
+    int encoderID;
+    int buttonL;
+    int buttonR;
+};
+
+
+#endif
